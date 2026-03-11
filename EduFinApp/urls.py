87cb5209e6e3ac696bad37549e4a5b25897a69b8
MASHA 250K <yoursvideos596@gmail.com>
2026-03-11 14:58:22 +0200

    Step 9

--- EduFinApp/urls.py
+++ EduFinApp/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import testing_view, health_check  
+from core.views import testing_view, testing_detail_view, health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testing/', testing_view, name='testing'),
+    path('testing/<int:id>/', testing_detail_view, name='testing_detail'),
     path('health/', health_check, name='health'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
 from django.http import JsonResponse
 from core.models import Testing
+from django.shortcuts import get_object_or_404 
 from core.serializers import TestingSerializer
 
 def testing_view(request):
     testing_records = Testing.objects.all()
     serializer = TestingSerializer(testing_records, many=True)
     return JsonResponse(serializer.data, safe=False)
+
+def testing_detail_view(request, id):
+    testing = get_object_or_404(Testing, id=id)
+    serializer = TestingSerializer(testing)
+    return JsonResponse(serializer.data)
 def health_check(request):
     return JsonResponse({'status': 'ok'})
